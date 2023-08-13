a11f30865aeffb8cb988446f29023f177fea2c49
Bartha Zoltán <barthazoli00@gmail.com>
2023-08-13 21:43:30 +0200

    Add editing and creation

--- frontend/src/BloggerEdit.js
+++ frontend/src/BloggerEdit.js
@@ -34,6 +34,8 @@ class BloggerEdit extends Component {
         const name = target.name;
         let item = {...this.state.item};
         item[name] = value;
+
+        console.log(item)
         this.setState({item});
     }
 
@@ -50,6 +52,44 @@ class BloggerEdit extends Component {
             body: JSON.stringify(item),
         });
         this.props.history.push('/bloggers');
+    }
+
+    render() {
+        const {item} = this.state;
+        const title = <h2>{item.id ? 'Edit Client' : 'Add Client'}</h2>;
+
+        return <div>
+            <AppNavbar/>
+            <Container>
+                {title}
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label for="userName">Name</Label>
+                        <Input type="text" name="userName" id="userName" value={item.userName || ''}
+                               onChange={this.handleChange} autoComplete="name"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email">Email</Label>
+                        <Input type="email" name="email" id="email" value={item.email || ''}
+                               onChange={this.handleChange} autoComplete="email"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="password">Password</Label>
+                        <Input type="password" name="password" id="password" value={item.password || ''}
+                               onChange={this.handleChange} autoComplete="password"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="avatar">Avatar</Label>
+                        <Input type="text" name="avatar" id="avatar" value={item.avatar || ''}
+                               onChange={this.handleChange} autoComplete="avatar"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Button color="primary" type="submit">Save</Button>{' '}
+                        <Button color="secondary" tag={Link} to="/clients">Cancel</Button>
+                    </FormGroup>
+                </Form>
+            </Container>
+        </div>
     }
 }
 export default withRouter(BloggerEdit);
